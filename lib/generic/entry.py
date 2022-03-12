1b5806e17f61de837ca2d0e9d029acc73028fec1
Nathan Menge <nathan.menge@gmail.com>
2022-03-12 16:33:13 -0500

    [Generic] Removed non-ID fields from generics so that it works generically.

--- lib/generic/entry.py
+++ lib/generic/entry.py
@@ -3,7 +3,7 @@ from gql import gql
 
 def delete(client, table: str, id_):
     request = (f"mutation delete ($id: ID!) "
-               f"{{delete{table}(input: {{id: $id}}) {{ name }} }}")
+               f"{{delete{table}(input: {{id: $id}}) {{ id }} }}")
     response = client.execute(gql(request), variable_values={'id': id_})
     return response
 
